afd5e8a7e04e68c38792d9878fdd0a8a55e7e854
MarkNou <mark.nou95@gmail.com>
2023-05-10 21:56:07 -0700

    frontend and backend completed

--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -1,23 +1,47 @@
-import React from "react"
-import { Button, Input, Label } from "reactstrap"
-import ModalComponent from "./components/ModalComponent"
-import "./App.css"
+import React from "react";
+import { Button, Input, Label } from "reactstrap";
+import ModalComponent from "./components/ModalComponent";
+import "./App.css";
+import { useState } from "react";
 
 const App = () => {
+  const [form, setForm] = useState({
+    name: "",
+  });
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const reset = () => {
+    setForm({
+      name: "",
+    });
+  };
+
+  
+
   return (
+    <body>
     <div className="entire-content">
-      <h1>Preparedness Assessment</h1>
+      <h1 className="heading">Preparedness Assessment</h1>
       <div className="form">
         <div className="input">
-          <Label for="name">Enter your name</Label>
-          <Input />
+          <Label className="label" for="name">Enter your name</Label>
+          <Input className="input" name="name" onChange={handleChange} value={form.name} />
         </div>
-        <Button>Click Me</Button>
-        <Button>Reset</Button>
-        <ModalComponent />
+        <div className="buttons">
+        <ModalComponent form = { form.name } />
+        <Button className="button" style={{ backgroundColor: '#0dcaf0' }} onClick={reset} name="reset button">
+          Reset
+        </Button>
+        </div>
+        
       </div>
     </div>
-  )
-}
+    </body>
+  );
+};
 
-export default App
+export default App;
